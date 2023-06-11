b0cc31e149e67dbfa9017b07eea237898c9b2bdd
Pratyush-Ver <pratyushverma.iitrpr@gmail.com>
2023-06-11 19:03:58 +0530

    added functions

--- newdetect.py
+++ newdetect.py
@@ -5,23 +5,9 @@ import numpy as np
 import json
 from datetime import datetime
 import os
+import time
  
-# Get the list of all files and directories
-path = "C://Users//praty//Desktop//work//scriptsAudio//rec"
-dir_list = os.listdir(path)
- 
-print("Files and directories in '", path, "' :")
- 
-# prints all files
-print(dir_list)
-
-# datetime object containing current date and time
-
-#print("date and time =", dt_string)
-#filename="rec_2023-06-10T14-37-00.wav"
-recDuration="20"
-recSamplingRate="48000"
-recFormat="wav"
+path = "C://Users//praty//Desktop//work//scriptsAudio//rec" 
 
 # Data to be written
 payload = {
@@ -40,8 +26,6 @@ payload = {
 "peakTimeStamp":[],
 "peakMagnitude":[]
 }
-
-# Serializing json
 
 def build_payload(onset_detects,onset_detects_values,filename):
     now = datetime.now()
@@ -63,46 +47,71 @@ def json_write(payload):
     json_object = json.dumps(payload, indent=4)
     with open("detections.json", "a") as outfile:
         outfile.write(json_object)
+
+def filecheck():
+    dir_list = os.listdir(path)
+    print("Files and directories in '", path, "' :")
+    #print(dir_list)
+    if len(dir_list)==0:
+        return False, dir_list
+    else:
+        return True, dir_list
+
+#sampling rate is variable
+
+# Get the list of all files and directories
+#function for file list
+#return true if files detected with list else return false
+#once file is processed, delete 
+
+#filename="rec_2023-06-10T14-37-00.wav"
+recDuration="20"
+recSamplingRate="48000"
+recFormat="wav"
+
+#x=list(range(1,220161))
+while 1:
+    flag, dir_list=filecheck()
+    #logic for waiting until new files are generated
+    if flag==False:
+        print("No files... Sleeping...")
+        time.sleep(30)
+    else:
+        #dir_list=filecheck()
+        for file in dir_list:
+            # Load audio file
+            audio_file = file
+            y_init, sr_init = librosa.load(audio_file)
+            print("Files found! sr is ",sr_init)
+            #print("y is",y_init)
+            # Plot waveform of amplitude vs time
+            #plt.figure(figsize=(14, 5))
+            #fig, ax = plt.subplots(nrows=3, sharex=True)
+
+            # librosa.display.waveshow(y_init, sr=sr_init, ax=ax[0])
+            # ax[0].set(title='Envelope view, mono')
+            # ax[0].label_outer()
+
+            # plt.plot(y_init,x)
+            # plt.show
+
+            # Set loudness threshold
+            threshold = 0.05
+
+            # Find timestamps when the sound goes over the threshold
+            onset_frames = librosa.onset.onset_detect(y=y_init, sr=sr_init)
+            onset_times = librosa.frames_to_time(onset_frames, sr=sr_init)
+            #print(y_init.shape)
+            #print("onset times are ",onset_times,type(onset_times))
+            onset_detects=[]
+            onset_detects_values=[]
+            # Print timestamps when the sound goes over the threshold
+            for onset_time in onset_times:
+                if int(onset_time * sr_init) < len(y_init) and y_init[int(onset_time * sr_init)] > threshold:
+                    #print(onset_time)
+                    onset_detects.append(str(onset_time))
+                    onset_detects_values.append(str(y_init[int(onset_time * sr_init)]))
+            print("detected time are",onset_detects)
+            print("detected values are",onset_detects_values)
+            build_payload(onset_detects,onset_detects_values,file)
         
-
-
-x=list(range(1,220161))
-
-#logic for waiting until new files are generated
-
-for file in dir_list:
-    # Load audio file
-    audio_file = file
-    y_init, sr_init = librosa.load(audio_file)
-    print("sr is ",sr_init)
-    #print("y is",y_init)
-    # Plot waveform of amplitude vs time
-    #plt.figure(figsize=(14, 5))
-    fig, ax = plt.subplots(nrows=3, sharex=True)
-
-    librosa.display.waveshow(y_init, sr=sr_init, ax=ax[0])
-    ax[0].set(title='Envelope view, mono')
-    ax[0].label_outer()
-
-    # plt.plot(y_init,x)
-    # plt.show
-
-    # Set loudness threshold
-    threshold = 0.05
-
-    # Find timestamps when the sound goes over the threshold
-    onset_frames = librosa.onset.onset_detect(y=y_init, sr=sr_init)
-    onset_times = librosa.frames_to_time(onset_frames, sr=sr_init)
-    #print(y_init.shape)
-    #print("onset times are ",onset_times,type(onset_times))
-    onset_detects=[]
-    onset_detects_values=[]
-    # Print timestamps when the sound goes over the threshold
-    for onset_time in onset_times:
-        if int(onset_time * sr_init) < len(y_init) and y_init[int(onset_time * sr_init)] > threshold:
-            #print(onset_time)
-            onset_detects.append(str(onset_time))
-            onset_detects_values.append(str(y_init[int(onset_time * sr_init)]))
-    print("detected time are",onset_detects)
-    print("detected values are",onset_detects_values)
-    build_payload(onset_detects,onset_detects_values,file)
--- recorder.sh
+++ recorder.sh
@@ -1,7 +1,39 @@
 #!/bin/sh
-#sound card verification
-#card number identification
-#
+#!/bin/sh
+card=`arecord -l | grep card | awk '{print $2}' | sed 's/://g'`
+device=`arecord -l | grep card | awk '{print $8}' | sed 's/://g'`
+echo "$card"
+echo "$device"
+duration=10
+mic=0
+stamp() {
+    TEXT="rec"
+    DATE=`date +%d-%m-%Y`
+    TIME=`date +%H-%M-%S`
+    #ZONE=`date +"%Z %z"`
+    echo ${TEXT}_${DATE}T${TIME}
+}
 
-card = $(arecord -l | grep card | awk '{print $2}' | sed 's/://g';)
-device = $(arecord -l | grep card | awk '{print $8}' | sed 's/://g';)
+
+miccheck() {
+    card=`arecord -l | grep card | awk '{print $2}' | sed 's/://g'`
+    device=`arecord -l | grep card | awk '{print $8}' | sed 's/://g'`
+    if [ -z "$card" ]
+        then
+        echo "No mic detected"
+        mic=0
+        exit 1
+        # execute command here
+    else
+        echo "Mic detected!"
+        mic=1
+    fi
+}
+miccheck
+while [ $mic -eq 1 ]
+do
+    miccheck
+    filename=`stamp`
+    arecord -D hw:$card,$device -d $duration -f S16_LE -r 48000 -t wav -c 1 $filename.wav
+    sleep 0.5
+done
